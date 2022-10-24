c0a5df9f78973a90fccefba4a7baf8d7e0d11902
Artem_Loseu <alovakby@gmail.com>
2022-10-24 15:13:10 +0300

    first commit

---
+++ src/api/config.js
@@ -0,0 +1,42 @@
+import axios from "axios";
+import { LOCAL_STORAGE_KEYS } from "../constants/localStorageKeys";
+import AuthService from "../services/authService";
+
+// const BASE_URL = "https://it-shatle-demo-api.herokuapp.com";
+const BASE_URL =
+  "https://timely-mochi-9018a9.netlify.app/.netlify/functions/server";
+
+const config = {
+  baseURL: BASE_URL,
+  headers: {
+    "Access-Control-Allow-Origin": "http://localhost:3002/",
+  },
+};
+
+const api = axios.create(config);
+
+api.interceptors.request.use((axiosConfig) => {
+  const accessToken = localStorage.getItem(LOCAL_STORAGE_KEYS.ACCESS_TOKEN);
+
+  axiosConfig.headers.Authorization = `Bearer ${accessToken}`;
+
+  return axiosConfig;
+});
+
+api.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  (error) => {
+    if (error.response.status === 401) {
+      AuthService.signOut();
+      return;
+    }
+    if (error.response.status === 400) {
+      return Promise.reject(error.response.data);
+    }
+    return Promise.reject(error.response.data);
+  }
+);
+
+export default api;
