2ab517cc905a910f8dfd0dae428481d6f1582f1d
Artem_Loseu <alovakby@gmail.com>
2022-10-25 17:54:46 +0300

    fixed

--- src/api/config.js
+++ src/api/config.js
@@ -3,14 +3,17 @@ import { LOCAL_STORAGE_KEYS } from "../constants/localStorageKeys";
 import AuthService from "../services/authService";
 
 // const BASE_URL = "https://it-shatle-demo-api.herokuapp.com";
-const BASE_URL =
-  "https://timely-mochi-9018a9.netlify.app/.netlify/functions/server";
-// const BASE_URL = "http://localhost:5000/.netlify/functions/server";
+// const BASE_URL =
+//   "https://timely-mochi-9018a9.netlify.app/.netlify/functions/server";
+const BASE_URL = "http://localhost:5000/.netlify/functions/server";
 
 const config = {
   baseURL: BASE_URL,
   headers: {
-    "Access-Control-Allow-Origin": "https://timely-mochi-9018a9.netlify.app",
+    Origin: [
+      "http://localhost:3002",
+      "https://singular-ganache-ea177f.netlify.app/",
+    ],
   },
   withCredentials: true,
 };
